0765ed84c258c8007b5f3212950c92d5c81ee38c
erickd <airik.sutanto@beeso.io>
2022-04-30 22:29:36 +0700

    view alaert

---
+++ lib/viewAlerts.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ViewAlerts extends StatefulWidget {
+  const ViewAlerts({Key? key}) : super(key: key);
+
+  @override
+  State<ViewAlerts> createState() => _ViewAlertsState();
+}
+
+class _ViewAlertsState extends State<ViewAlerts> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
