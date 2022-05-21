f1004336ac904f40107e26187dcf5b5de0ce60c5
erickd <airik.sutanto@beeso.io>
2022-05-21 16:19:29 +0700

    update save progress ...

--- lib/SecondScreen.dart
+++ lib/SecondScreen.dart
@@ -4,9 +4,11 @@ import 'dart:io';
 import 'package:bruxism2/ViewQuestion.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:localstorage/localstorage.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:survey_kit/survey_kit.dart';
 
 class CounterStorage {
   Future<String> get _localPath async {
@@ -155,7 +157,9 @@ class _SecondScreenState extends State<SecondScreen> {
               list.removeWhere((element) => element.genId == context2.genId);
               storage.setItem("questions", json.encode(list));
             }
-          } catch (ex) {}
+          } catch (ex) {
+            print(ex);
+          }
 
           Navigator.pop(context);
         },
@@ -186,49 +190,220 @@ class _SecondScreenState extends State<SecondScreen> {
       Navigator.pop(context);
     }
 
+    Future<Task> getSampleTask() {
+      var task = NavigableTask(
+        id: TaskIdentifier(),
+        steps: [
+          InstructionStep(
+            title: 'Welcome to the\nQuickBird Studios\nHealth Survey',
+            text: 'Get ready for a bunch of super random questions!',
+            buttonText: 'Let\'s go!',
+          ),
+          QuestionStep(
+            title: 'How old are you?',
+            answerFormat: const IntegerAnswerFormat(
+              defaultValue: 25,
+              hint: 'Please enter your age',
+            ),
+            isOptional: true,
+          ),
+          QuestionStep(
+            title: 'Medication?',
+            text: 'Are you using any medication',
+            answerFormat: const BooleanAnswerFormat(
+              positiveAnswer: 'Yes',
+              negativeAnswer: 'No',
+              result: BooleanResult.POSITIVE,
+            ),
+          ),
+          QuestionStep(
+            title: 'Tell us about you',
+            text:
+                'Tell us about yourself and why you want to improve your health.',
+            answerFormat: const TextAnswerFormat(
+              maxLines: 5,
+              validationRegEx: "^(?!\s*\$).+",
+            ),
+          ),
+          QuestionStep(
+            title: 'Select your body type',
+            answerFormat: const ScaleAnswerFormat(
+              step: 1,
+              minimumValue: 1,
+              maximumValue: 5,
+              defaultValue: 3,
+              minimumValueDescription: '1',
+              maximumValueDescription: '5',
+            ),
+          ),
+          QuestionStep(
+            title: 'Known allergies',
+            text: 'Do you have any allergies that we should be aware of?',
+            isOptional: false,
+            answerFormat: const MultipleChoiceAnswerFormat(
+              textChoices: [
+                TextChoice(text: 'Penicillin', value: 'Penicillin'),
+                TextChoice(text: 'Latex', value: 'Latex'),
+                TextChoice(text: 'Pet', value: 'Pet'),
+                TextChoice(text: 'Pollen', value: 'Pollen'),
+              ],
+            ),
+          ),
+          QuestionStep(
+            title: 'Done?',
+            text: 'We are done, do you mind to tell us more about yourself?',
+            isOptional: true,
+            answerFormat: const SingleChoiceAnswerFormat(
+              textChoices: [
+                TextChoice(text: 'Yes', value: 'Yes'),
+                TextChoice(text: 'No', value: 'No'),
+              ],
+              defaultSelection: TextChoice(text: 'No', value: 'No'),
+            ),
+          ),
+          QuestionStep(
+            title: 'When did you wake up?',
+            answerFormat: const TimeAnswerFormat(
+              defaultValue: TimeOfDay(
+                hour: 12,
+                minute: 0,
+              ),
+            ),
+          ),
+          QuestionStep(
+            title: 'When was your last holiday?',
+            answerFormat: DateAnswerFormat(
+              minDate: DateTime.utc(1970),
+              defaultDate: DateTime.now(),
+              maxDate: DateTime.now(),
+            ),
+          ),
+          CompletionStep(
+            stepIdentifier: StepIdentifier(id: '321'),
+            text: 'Thanks for taking the survey, we will contact you soon!',
+            title: 'Done!',
+            buttonText: 'Submit survey',
+          ),
+        ],
+      );
+      task.addNavigationRule(
+        forTriggerStepIdentifier: task.steps[6].stepIdentifier,
+        navigationRule: ConditionalNavigationRule(
+          resultToStepIdentifierMapper: (input) {
+            switch (input) {
+              case "Yes":
+                return task.steps[0].stepIdentifier;
+              case "No":
+                return task.steps[7].stepIdentifier;
+              default:
+                return null;
+            }
+          },
+        ),
+      );
+      return Future.value(task);
+    }
+
     return Scaffold(
       appBar: AppBar(title: Text(widget.title)),
       body: Container(
-          padding: const EdgeInsets.all(10.0),
-          child: ListView.builder(
-            itemBuilder: (body, index) {
-              var data = context2["listQuestions"][index];
-              var items = data["option"].join(', ');
-
-              return Container(
-                padding: const EdgeInsets.all(10.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ViewQuestion(data: data, index: index),
-                    // Text(data["question"],
-                    //     style: const TextStyle(
-                    //         fontSize: 16.0, fontWeight: FontWeight.bold)),
-                    // Column(
-                    //   crossAxisAlignment: CrossAxisAlignment.start,
-                    //   children: List<Widget>.generate(
-                    //       data["option"].length,
-                    //       (int i) => ListTile(
-                    //             title: Text(data["option"][i]),
-                    //             leading: Radio<String>(
-                    //               value: index.toString() + "_" + i.toString(),
-                    //               groupValue: groupValue,
-                    //               onChanged: (String? val) {
-                    //                 setState(() {
-                    //                   groupValue = val!;
-                    //                   // radioButtonItem = 'ONE';
-                    //                   // id = 1;
-                    //                 });
-                    //               },
-                    //             ),
-                    //           )),
-                    // )
-                  ],
+        color: Colors.white,
+        child: FutureBuilder<Task>(
+          future: getSampleTask(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done &&
+                snapshot.hasData &&
+                snapshot.data != null) {
+              final task = snapshot.data!;
+              return SurveyKit(
+                task: task,
+                onResult: (SurveyResult result) {
+                  print(result.finishReason);
+                },
+                showProgress: true,
+                localizations: const {'cancel': 'Cancel', 'next': 'Next'},
+                themeData: Theme.of(context).copyWith(
+                  colorScheme:
+                      ColorScheme.fromSwatch(primarySwatch: Colors.cyan)
+                          .copyWith(onPrimary: Colors.white),
+                  primaryColor: Colors.cyan,
+                  backgroundColor: Colors.white,
+                  appBarTheme: const AppBarTheme(
+                    color: Colors.white,
+                    iconTheme: IconThemeData(color: Colors.cyan),
+                    titleTextStyle: TextStyle(color: Colors.cyan),
+                  ),
+                  iconTheme: const IconThemeData(color: Colors.cyan),
+                  textSelectionTheme: const TextSelectionThemeData(
+                      cursorColor: Colors.cyan,
+                      selectionColor: Colors.cyan,
+                      selectionHandleColor: Colors.cyan),
+                  cupertinoOverrideTheme:
+                      const CupertinoThemeData(primaryColor: Colors.cyan),
+                  outlinedButtonTheme: const OutlinedButtonThemeData(),
+                  textButtonTheme: TextButtonThemeData(
+                    style: ButtonStyle(
+                      textStyle: MaterialStateProperty.all(
+                        Theme.of(context).textTheme.button?.copyWith(
+                              color: Colors.cyan,
+                            ),
+                      ),
+                    ),
+                  ),
+                  textTheme: const TextTheme(
+                    headline2: TextStyle(
+                      fontSize: 28.0,
+                      color: Colors.black,
+                    ),
+                    headline5: TextStyle(
+                      fontSize: 24.0,
+                      color: Colors.black,
+                    ),
+                    bodyText2: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.black,
+                    ),
+                    subtitle1: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.black,
+                    ),
+                  ),
+                  inputDecorationTheme: const InputDecorationTheme(
+                    labelStyle: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                surveyProgressbarConfiguration: SurveyProgressConfiguration(
+                  backgroundColor: Colors.white,
                 ),
               );
-            },
-            itemCount: context2["listQuestions"].length,
-          )),
+            }
+            return const CircularProgressIndicator.adaptive();
+          },
+        ),
+      ),
+
+      // Container(
+      //     padding: const EdgeInsets.all(10.0),
+      //     child: ListView.builder(
+      //       itemBuilder: (body, index) {
+      //         var data = context2["listQuestions"][index];
+      //         var items = data["option"].join(', ');
+
+      //         return Container(
+      //           padding: const EdgeInsets.all(10.0),
+      //           child: Column(
+      //             crossAxisAlignment: CrossAxisAlignment.start,
+      //             children: [
+      //               ViewQuestion(data: data, index: index),
+
+      //             ],
+      //           ),
+      //         );
+      //       },
+      //       itemCount: context2["listQuestions"].length,
+      //     )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           // print("FACE...");
