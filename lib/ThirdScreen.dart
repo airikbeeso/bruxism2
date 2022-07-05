20ecc5e9f64d1d7fbd6ed596f37519eda254d8c9
erickd <airik.sutanto@beeso.io>
2022-07-05 18:56:04 +0700

    update save progress

--- lib/ThirdScreen.dart
+++ lib/ThirdScreen.dart
@@ -1,3 +1,12 @@
+import 'dart:convert';
+import 'dart:ffi';
+import 'dart:io';
+import 'dart:math';
+
+import 'package:bruxism2/LocalNotifyManager.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:glassmorphism/glassmorphism.dart';
 import 'package:localstorage/localstorage.dart';
@@ -14,12 +23,133 @@ class ThirdScreen extends StatefulWidget {
 class _ThirdScreenState extends State<ThirdScreen> {
   final LocalStorage storage = LocalStorage('questions');
   bool initialized = false;
+  String userId = "";
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     var items = storage.getItem("initial_date_time");
-    storage.ready.then((value) => log(value.toString()));
+    storage.ready.then((value) => print(value.toString()));
+    Firebase.initializeApp()
+        .then((value) => userId = FirebaseAuth.instance.currentUser!.uid);
+  }
+
+  Future<dynamic> checkForAnswer(String id) async {
+    return FirebaseFirestore.instance.collection("alerts").doc(id).get();
+  }
+
+  openQuestion(int time) async {
+    var dt = DateTime.fromMillisecondsSinceEpoch(time);
+    var packOfQuestions = [
+      {
+        "id": 0,
+        "question": "Kondisi gigi geligi anda saat ini",
+        "option": [
+          "Terpisah",
+          "Berkontak ringan",
+          "Berkontak erat",
+          "Bergemeretak"
+        ],
+        "form": "radio"
+      },
+      {
+        "id": 1,
+        "question": "Kondisi otot wajah/rahang (?) anda saat ini",
+        "option": [
+          "Rileks",
+          "Otot wajah/rahang tegang dan rahang terasa kencang tanpa ada gigi yang berkontak"
+        ],
+        "form": "radio"
+      },
+      {
+        "id": 2,
+        "question": "Apakah anda merasakan nyeri di daerah wajah",
+        "option": ["Ya", "Tidak"],
+        "form": "radio"
+      },
+      {
+        "id": 3,
+        "question": "Bila nyeri, berapa skala nyeri anda?",
+        "option": 5,
+        "form": "scale"
+      }
+    ];
+
+    var packOfQuestions2 = [
+      {
+        "id": 0,
+        "question": "Kondisi gigi geligi anda saat ini",
+        "option": [
+          "Terpisah",
+          "Berkontak ringan",
+          "Berkontak erat",
+          "Bergemeretak"
+        ],
+        "form": "radio"
+      },
+      {
+        "id": 1,
+        "question": "Kondisi otot wajah/rahang (?) anda saat ini",
+        "option": [
+          "Rileks",
+          "Otot wajah/rahang tegang dan rahang terasa kencang tanpa ada gigi yang berkontak"
+        ],
+        "form": "radio"
+      },
+      {
+        "id": 2,
+        "question": "Apakah anda merasakan nyeri di daerah wajah",
+        "option": ["Ya", "Tidak"],
+        "form": "radio"
+      },
+      {
+        "id": 3,
+        "question": "Kondisi anda hari ini",
+        "option": [
+          "Merasa gugup atau tegang",
+          "Sulit mengontrol kawatir",
+          "Merasa sedih, depresi",
+          "Merasa malas melakukan sesuatu"
+        ],
+        "form": "check"
+      }
+    ];
+
+    var rng = Random();
+    int rn = rng.nextInt(100);
+    var chosenQuestion = rn % 2 == 0 ? packOfQuestions2 : packOfQuestions;
+    var inputFormat = DateFormat('dd/MM/yyyy HH:mm');
+
+    var inputDate = inputFormat
+        .parse('${dt.day}/${dt.month}/${dt.year} ${dt.hour.toString()}:00');
+    var genId =
+        "${dt.year}-${dt.month}-${dt.day}-${dt.hour.toString()}-${FirebaseAuth.instance.currentUser!.uid}";
+    var context = {
+      "mode": dt.hour.toString(),
+      "ih": dt.hour,
+      "im": dt.minute,
+      "is": dt.second,
+      "init": dt.millisecondsSinceEpoch,
+      "end": inputDate.millisecondsSinceEpoch,
+      "date": dt.toIso8601String(),
+      "timestamp": dt.millisecondsSinceEpoch,
+      "status": "onSchedule",
+      "question": "default question",
+      "answer": "default answer",
+      "listQuestions": chosenQuestion,
+      'userId': FirebaseAuth.instance.currentUser!.uid,
+      'genId': genId,
+      'email': FirebaseAuth.instance.currentUser!.email,
+      'name': FirebaseAuth.instance.currentUser!.displayName,
+      'answerOn': 0
+    };
+    await LocalNotifyManager.init().dailyAtTimeNotification(
+        rng.nextInt(1000),
+        dt.hour,
+        dt,
+        jsonEncode(context),
+        "Bruxism Notificaiton",
+        "Rate your pain, Jam ${dt.hour}");
   }
 
   @override
@@ -65,12 +195,88 @@ class _ThirdScreenState extends State<ThirdScreen> {
                 if (!initialized) {
                   var items = storage.getItem("initial_date_time");
                   if (items != null) {
+                    var current = DateTime(2022, 8, 5, 17, 0, 0);
+                    var before = DateTime.fromMillisecondsSinceEpoch(items);
+                    print("userId: $userId");
+                    var dayAdd = 0;
+
+                    bool bb = false;
+                    var b = before.add(Duration(days: dayAdd));
+
+                    print("${b.month} ${b.day}");
+
+                    var first = DateTime(b.year, b.month, b.day, 9, 0, 0);
+                    var second = DateTime(b.year, b.month, b.day, 12, 0, 0);
+                    var third = DateTime(b.year, b.month, b.day, 15, 0, 0);
+                    var forth = DateTime(b.year, b.month, b.day, 18, 0, 0);
+                    var fifth = DateTime(b.year, b.month, b.day, 21, 0, 0);
+
+                    if (b.millisecondsSinceEpoch <
+                            first.millisecondsSinceEpoch &&
+                        first.millisecondsSinceEpoch <
+                            current.millisecondsSinceEpoch) {
+                      var id = userId +
+                          "_" +
+                          first.millisecondsSinceEpoch.toString();
+                      print("IKD $id");
+                      checkForAnswer(id)
+                          .then((value) => print("mmmmm 2 $value"));
+                    }
+
+                    if (b.millisecondsSinceEpoch <
+                            second.millisecondsSinceEpoch &&
+                        second.millisecondsSinceEpoch <
+                            current.millisecondsSinceEpoch) {
+                      var id = userId +
+                          "_" +
+                          second.millisecondsSinceEpoch.toString();
+                      print("IKD $id");
+                      checkForAnswer(id)
+                          .then((value) => print("mmmmm 3 $value"));
+                    }
+
+                    if (b.millisecondsSinceEpoch <
+                            third.millisecondsSinceEpoch &&
+                        third.millisecondsSinceEpoch <
+                            current.millisecondsSinceEpoch) {
+                      var id = userId +
+                          "_" +
+                          third.millisecondsSinceEpoch.toString();
+                      print("IKD $id");
+                      checkForAnswer(id)
+                          .then((value) => print("mmmmm 4 $value"));
+                    }
+
+                    if (b.millisecondsSinceEpoch <
+                            forth.millisecondsSinceEpoch &&
+                        forth.millisecondsSinceEpoch <
+                            current.millisecondsSinceEpoch) {
+                      var id = userId +
+                          "_" +
+                          forth.millisecondsSinceEpoch.toString();
+                      print("IKD $id");
+                      checkForAnswer(id)
+                          .then((value) => print("mmmmm 5 $value"));
+                    }
+
+                    if (b.millisecondsSinceEpoch <
+                            fifth.millisecondsSinceEpoch &&
+                        fifth.millisecondsSinceEpoch <
+                            current.millisecondsSinceEpoch) {
+                      var id = userId +
+                          "_" +
+                          fifth.millisecondsSinceEpoch.toString();
+                      print("IKD $id");
+                      checkForAnswer(id)
+                          .then((value) => {if (null == value.data()) {}});
+                    }
+
                     return Text(DateFormat('MM/dd/yyyy hh:mm a')
                         .format(DateTime.fromMillisecondsSinceEpoch(items)));
                   }
                   initialized = true;
                 }
-                return const Text("Wwww");
+                return const Text("nothing");
               }),
         ));
     // Scaffold(
